c179013870303f5951a5b2dbe73f240d5e627c44
akaur-begdev <akaur@qurioskill.ca>
2025-09-06 11:45:09 -0400

    1st iteration

--- utils.py
+++ utils.py
@@ -1,12 +1,3 @@
-"""Create a utils.py file which has the following functions:
-save_password(entity_name, password)
-Takes in an entity name and password and saves it in a file.
-The name of the file should in a file named “<entitiy_name>.passwd”
-retrieve_password(entity_name)
-Retrieves the password for a particular entity for its password file.
-If the entitiy’s password file is not found, returns “error”
-"""
-
 import io
 import os
 
